23001e06b4c3c753d1972bd5a59811b03776ff74
IliaManolov <81464524+iliamanolov@users.noreply.github.com>
2021-04-19 15:57:24 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,93 +1,36 @@
-:root {
-  --primary-color: #5fbaa7;
+const currencyEl_one = document.getElementById('currency-one');
+const amountEl_one = document.getElementById('amount-one');
+const currencyEl_two = document.getElementById('currency-two');
+const amountEl_two = document.getElementById('amount-two');
+const rateEl = document.getElementById('rate');
+const swap = document.getElementById('swap');
+
+function calculate() {
+  const currency_one = currencyEl_one.value;
+  const currency_two = currencyEl_two.value;
+  fetch("https://open.exchangerate-api.com/v6/latest")
+    .then(res => res.json())
+    .then(data => {
+      //  console.log(data);
+      const rate = data.rates[currency_two] / data.rates[currency_one];
+      rateEl.innerText = `1 ${currency_one} = ${rate} ${currency_two}`;
+      amountEl_two.value = (amountEl_one.value * (rate)).toFixed(2);
+    });
 }
 
-* {
-  box-sizing: border-box;
-}
 
-body {
-  background-color: #f4f4f4;
-  font-family: Arial, Helvetica, sans-serif;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  height: 100vh;
-  margin: 0;
-  padding: 20px;
-}
+// Event Listener
+currencyEl_one.addEventListener('change', calculate);
+amountEl_one.addEventListener('input', calculate);
+currencyEl_two.addEventListener('change', calculate);
+amountEl_two.addEventListener('input', calculate);
 
-h1 {
-  color: var(--primary-color);
-}
+swap.addEventListener('click', () => {
+  const temp = currencyEl_one.value;
+  currencyEl_one.value = currencyEl_two.value;
+  currencyEl_two.value = temp;
+  calculate();
+});
 
-p {
-  text-align: center;
-}
 
-.btn {
-  color: #fff;
-  background: var(--primary-color);
-  cursor: pointer;
-  border-radius: 5px;
-  font-size: 12px;
-  padding: 5px 12px;
-}
-
-.money-img {
-  width: 150px;
-}
-
-.currency {
-  padding: 40px 0;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-}
-
-.currency select {
-  padding: 10px 20px 10px 10px;
-  -moz-appearance: none;
-  -webkit-appearance: none;
-  appearance: none;
-  border: 1px solid #dedede;
-  font-size: 16px;
-  /*  You may not need these following lines. The arrow did not show for me on MacOS/Chrome so I added it. Just remove it if you would like  */
-  background: transparent;
-  background-image: url('data:image/svg+xml;charset=US-ASCII,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%22292.4%22%20height%3D%22292.4%22%3E%3Cpath%20fill%3D%22%20000002%22%20d%3D%22M287%2069.4a17.6%2017.6%200%200%200-13-5.4H18.4c-5%200-9.3%201.8-12.9%205.4A17.6%2017.6%200%200%200%200%2082.2c0%205%201.8%209.3%205.4%2012.9l128%20127.9c3.6%203.6%207.8%205.4%2012.8%205.4s9.2-1.8%2012.8-5.4L287%2095c3.5-3.5%205.4-7.8%205.4-12.8%200-5-1.9-9.2-5.5-12.8z%22%2F%3E%3C%2Fsvg%3E');
-  background-position: right 10px top 50%, 0, 0;
-  background-size: 12px auto, 100%;
-  background-repeat: no-repeat;
-}
-
-.currency input {
-  border: 0;
-  background: transparent;
-  font-size: 30px;
-  text-align: right;
-}
-
-.swap-rate-container {
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-}
-
-.rate {
-  color: var(--primary-color);
-  font-size: 14px;
-  padding: 0 10px;
-}
-
-select:focus,
-input:focus,
-button:focus {
-  outline: 0;
-}
-
-@media (max-width: 600px) {
-  .currency input {
-    width: 200px;
-  }
-}
+calculate();
